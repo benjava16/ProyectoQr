8ca3904771591a73ce6ae454eec8a8b65c283af6
Plaza Vespucio <pvespucio@PVAWSB03LC1009.duoc.acad>
2024-10-23 11:26:42 -0300

    Todo subido (o no)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"appqr-database","appId":"1:120477303005:web:ce92fa4fe48c32560edb34","storageBucket":"appqr-database.appspot.com","apiKey":"AIzaSyAuVd6G46Jr16OZbYGdcZCZ6yovQntkCyA","authDomain":"appqr-database.firebaseapp.com","messagingSenderId":"120477303005","measurementId":"G-WMYN5ZCTYN"})), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"appqr-database","appId":"1:120477303005:web:ce92fa4fe48c32560edb34","storageBucket":"appqr-database.appspot.com","apiKey":"AIzaSyAuVd6G46Jr16OZbYGdcZCZ6yovQntkCyA","authDomain":"appqr-database.firebaseapp.com","messagingSenderId":"120477303005","measurementId":"G-WMYN5ZCTYN"})), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage()), provideFirebaseApp(() => initializeApp({"projectId":"appqr-database","appId":"1:120477303005:web:e3d1d036093dde7d0edb34","storageBucket":"appqr-database.appspot.com","apiKey":"AIzaSyAuVd6G46Jr16OZbYGdcZCZ6yovQntkCyA","authDomain":"appqr-database.firebaseapp.com","messagingSenderId":"120477303005","measurementId":"G-VBVSBLLLXC"})), provideAuth(() => getAuth()), UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
